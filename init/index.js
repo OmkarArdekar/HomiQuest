3e6dff5ddb7d0b01bc8009a7029982eb10278442
OmkarArdekar <omkarardekar12@gmail.com>
2025-03-27 16:15:06 +0530

    Added HomiQuest Project

--- init/index.js
+++ init/index.js
@@ -2,15 +2,14 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = process.env.MONGODB_URL;
-
-main()
-  .then(() => {
-    console.log("connected to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+const MONGO_URL = //MONGODB_URL;
+  main()
+    .then(() => {
+      console.log("connected to DB");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 
 async function main() {
   await mongoose.connect(MONGO_URL);
